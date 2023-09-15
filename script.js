cb4b8b7d05cb1287c70b9a542ed27759c30bd8fa
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-15 11:30:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
 
+document.getElementById("addTodoBtn").addEventListener("click",(e)=>{
+	e.preventDefault;
+	let input = document.getElementById('newTodoInput').value.trim();
+	if(input == ""){
+		alert("Please enter a To-Do Event");
+		return;
+	}
+	const li = document.createElement("li");
+	li.innerText = input;
+	document.getElementById("todoList").appendChild(li);
+	document.getElementById('newTodoInput').value = "";
+})
